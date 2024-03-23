0ff4a561a1e672b9493ed8c8d0e72da8b1797147
william <williamnixon20@gmail.com>
2024-03-23 09:11:31 +0000

    copy_mpi.sh update

--- copy_mpi.sh
+++ copy_mpi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Path to your MPI executable
-MPI_EXECUTABLE="./mpi"
+MPI_EXECUTABLE="./bin"
 
 # Path to the directory where you want to copy the MPI executable on remote servers
 REMOTE_DIRECTORY="/home/k02-08"
@@ -10,9 +10,9 @@ REMOTE_DIRECTORY="/home/k02-08"
 while read -r host; do
     echo "Copying to $host"
     # Copy MPI executable to the remote server using scp
-    scp "$MPI_EXECUTABLE" "$host":"$REMOTE_DIRECTORY"
+    scp -r "$MPI_EXECUTABLE" "$host":"$REMOTE_DIRECTORY"
     scp "./hostfile" "$host":"$REMOTE_DIRECTORY"
-    scp "./mpi.c" "$host":"$REMOTE_DIRECTORY"
+    scp -r "./src" "$host":"$REMOTE_DIRECTORY"
     scp -r "./test_cases" "$host":"$REMOTE_DIRECTORY"
     scp "copy_mpi.sh" "$host":"$REMOTE_DIRECTORY"
     echo "Copying to $host"
